21cd5853e654128115067cb21d3239169c6dcba9
Ray Klump <klumpra@lewisu.edu>
2022-09-21 16:32:25 -0500

    Added a second line.

--- mysecondapp/Another.java
+++ mysecondapp/Another.java
@@ -3,5 +3,6 @@ package mysecondapp;
 public class Another {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("This is cool.");
     }
 }
